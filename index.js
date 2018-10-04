952b87dc37de8d5c75f025bcb068a3b382dcd7f9
TJ <tarunjha@outlook.com>
2018-10-04 20:34:21 +0530

    Initialized repository

---
+++ index.js
@@ -0,0 +1,35 @@
+// Reverses a string.
+function reverse(string) {
+  return Array.from(string).reverse().join("");
+}
+
+function Phrase(content) {
+  this.content = content;
+
+//Can't read this directly
+  this.processor = function MakeLowerCase(string) {
+    return string.toLowerCase();
+  }
+
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === reverse(this.processedContent());
+  }
+}
+
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+}
+
+
+TranslatedPhrase.prototype = new Phrase();
