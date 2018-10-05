9974a9a81b2068322d0dad33b23321eb5c7ab563
TJ <tarunjha@outlook.com>
2018-10-05 10:15:27 +0530

    Package.json file created, index.js changed as per tutotial

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
-// Reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+module.exports = Phrase;
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
+// Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
 
-//Can't read this directly
-  this.processor = function MakeLowerCase(string) {
-    return string.toLowerCase();
-  }
-
+  // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+    return this.content.toLowerCase();
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent() === reverse(this.processedContent());
+    return this.processedContent() === this.processedContent().reverse();
   }
 }
-
-function TranslatedPhrase(content, translation) {
-  this.content = content;
-  this.translation = translation;
-
-  // Returns translation processed for palindrome testing.
-  this.processedContent = function processedContent() {
-    return this.processor(this.translation);
-  }
-}
-
-
-TranslatedPhrase.prototype = new Phrase();
